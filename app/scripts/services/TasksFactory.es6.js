bc913a6f707cc564db2436d1d8cbdf95b1194b91
Piotr Kowalski <piecioshka@gmail.com>
2015-10-18 17:28:18 +0200

    Support localStorage

--- app/scripts/services/TasksFactory.es6.js
+++ app/scripts/services/TasksFactory.es6.js
@@ -1,19 +1,43 @@
+let save = (item) => {
+    try {
+        localStorage.setItem('grepnet-tasks', JSON.stringify(item));
+    } catch (ignore) {
+    }
+};
+
+let load = () => {
+    var tasks = null;
+
+    try {
+        tasks = JSON.parse(localStorage.getItem('grepnet-tasks'));
+    } catch (ignore) {
+    }
+
+    return tasks || [];
+};
+
 export default () => {
-    let tasks = [];
+    let tasks = load();
 
     return {
         add: (task) => {
             task.status = 'new';
             tasks.unshift(task);
+
+            save(tasks);
         },
 
         edit: (index, task) => {
             task.status = 'new';
             tasks[index] = task;
+
+            save(tasks);
         },
 
         remove(index) {
             tasks.splice(index, 1);
+
+            save(tasks);
         },
 
         getAll: () => {
