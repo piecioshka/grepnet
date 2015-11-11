5933ee053df32793d07ae93895f94236f8d1daf2
Piotr Kowalski <piecioshka@gmail.com>
2015-11-11 16:31:33 +0100

    Support error handling

--- app/scripts/directives/TaskCard.es6.js
+++ app/scripts/directives/TaskCard.es6.js
@@ -106,6 +106,9 @@ class Controller {
             }
 
             this._save();
+        }, () => {
+            this.resetCountdown();
+            this.$scope.task.state = 'error';
         });
     }
 
