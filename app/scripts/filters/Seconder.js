3f1740f3c3312ef5a0487244027b38b9682cb2d7
Piotr Kowalski <piecioshka@gmail.com>
2021-01-19 14:05:47 +0100

    Ups...

--- app/scripts/filters/Seconder.js
+++ app/scripts/filters/Seconder.js
@@ -1,8 +1,8 @@
 export default () => {
     return (number) => {
-        const hours = Math.floor(number / 3600);
-        const minutes = Math.floor((number - (hours * 3600)) / 60);
-        const seconds = number - (hours * 3600) - (minutes * 60);
+        let hours = Math.floor(number / 3600);
+        let minutes = Math.floor((number - (hours * 3600)) / 60);
+        let seconds = number - (hours * 3600) - (minutes * 60);
 
         hours = String(hours).padLeft(2, '0');
         minutes = String(minutes).padLeft(2, '0');
